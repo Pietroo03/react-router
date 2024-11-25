4a2a20817baef24af1923535e11e2c92b4fea5bf
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-25 16:05:26 +0100

    add about page

---
+++ src/data/team.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'Roger',
+        image: '/procione-1.jpg',
+        role: 'CEO',
+        talent: "Il CEO ha la capacità di rubare l'attenzione di tutti con le sue mosse astute e irresistibilmente buffe.Ogni volta che l'azienda affronta una crisi, il procione può attivare questa abilità e improvvisamente lanciare un'incredibile serie di manovre esilaranti per distrarre i concorrenti e i dipendenti: da balli improbabili a tentativi di parlare in un dialetto incomprensibile. Questa distrazione così esilarante fa sì che tutti si dimentichino della crisi, portando a una temporanea vittoria dell'azienda. Può sembrare un completo disastro, ma in realtà, per qualche misterioso motivo, queste buffonate aumentano il morale e la produttività, dando un'incredibile spinta al team, che finisce per lavorare con più energia e creatività!"
+    },
+    {
+        id: 2,
+        name: 'Douglas',
+        image: '/procione-2.jpg',
+        role: 'Receptionist',
+        talent: "Ha un talento unico: ogni volta che qualcuno entra in ufficio, lui accidentalemente combina guai in modo esilarante, ma sorprendentemente efficace. Invece di accogliere i visitatori con formalità, il procione fa tutto sbagliato e imbranato, ma per qualche motivo funziona sempre. Nonostante tutte le sue disavventure, i clienti e i colleghi adorano la sua strana capacità di farli ridere e, strano ma vero, proprio grazie alla sua imbranataggine, crea un'atmosfera rilassata e amichevole che rende l'ambiente di lavoro più piacevole.Quando tutti sono un po' più a loro agio, l'azienda funziona meglio."
+    },
+    {
+        id: 3,
+        name: 'Miguel',
+        image: '/procione-3.jpg',
+        role: 'Web Developer',
+        talent: "Ha un'abilità straordinaria: riuscire a costruire siti web incredibili con il minimo sforzo possibile. Ogni volta che gli viene chiesto di sviluppare una nuova funzionalità, lui applica il suo 'Codice Magico del Minimale', che gli consente di scrivere solo poche righe di codice, ma fa in modo che il risultato sembri una soluzione perfetta e sofisticata. Quando gli viene richiesto di implementare un'area login, invece di creare un sistema complesso con sicurezza avanzata, lui imposta un campo di testo, un pulsante e... fa finta che il sistema di autenticazione sia 'un lavoro in corso' e che la password si trovi 'nel file README'. Inoltre, ha la straordinaria capacità di 'scoprire bug' che, misteriosamente, spariscono dopo aver fatto un rapido e casuale 'refresh' del browser.E quando il team gli chiede di fare delle modifiche urgenti, lui risponde sempre con una scusa su come sia 'lunga' la strada per trovare una 'soluzione elegante', ma finisce per consegnare sempre il lavoro in tempo...seppur con un pizzico di 'spaghetti code'."
+    },
+    {
+        id: 4,
+        name: 'Orlando',
+        image: '/procione-4.jpg',
+        role: 'Software Engineer',
+        talent: "Mangia sempre, ha una superpotenza gastronomica che gli permette di scrivere codice senza mai smettere di mangiare. Ogni volta che si siede alla scrivania, tira fuori una borsa piena di snack: patatine, biscotti, frutta secca, e magari una pizza fredda lasciata in frigo. La sua abilità speciale, 'Snack del Codice', gli consente di codificare meglio mentre mastica. Mentre scrive codice, i suoi snack misteriosamente sembrano dare al suo cervello una marcia in più: ogni pezzo di cioccolato o ogni cucchiaio di popcorn potenzia la sua creatività e risolve i bug più rapidamente.Anche se potrebbe sembrare che stia solo mangiando(e magari facendo delle pause per sgranocchiare un po' di noci), il suo flusso di lavoro è impeccabile. I colleghi iniziano a notare che ogni volta che gli chiedono di risolvere un problema complesso, lui trova sempre la soluzione...ma solo dopo aver finito di mangiare il suo snack del momento.La sua abilità non solo lo rende una macchina da codifica, ma fa anche sembrare che il 'cibo' sia una parte fondamentale della sua routine lavorativa."
+    }
+]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,56 @@
+import team from "../data/team"
 export default function About() {
     return (
 
         <>
 
-            <main className="text-center pt-3 vh-100">
-                <h2 className="mt-4 text-primary">Questo è il nostro Team</h2>
+            <main className="vh-100">
+                <div className="text-center pt-3 ">
+                    <h2 className="mt-4 text-primary">Questo è il nostro Team</h2>
+                </div>
+
+                <section className="posts py-5 bg-light">
+                    <div className="container">
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-4">
+
+                            {team.map((member, index) => (
+                                <div className="col" key={index}>
+
+                                    <div className="card h-100 shadow-sm border-light">
+                                        <img
+                                            src={member.image}
+                                            className="card-img-top img-fluid"
+                                            alt={member.name}
+                                            style={{ objectFit: 'cover', height: '500px' }}
+                                        />
+                                        <div className="card-body d-flex flex-column">
+                                            <h5 className="card-title text-truncate">
+                                                {member.name}
+                                            </h5>
+                                            <hr />
+                                            <div className="p-2">
+                                                <strong>Ruolo: </strong>
+                                                {member.role}
+                                            </div>
+                                            <hr />
+                                            <div className="card-text">
+                                                <p
+                                                    style={{ maxHeight: '120px', overflowX: 'hidden' }}>
+                                                    {member.talent}
+                                                </p>
+                                            </div>
+                                            <hr />
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+
+                </section>
             </main>
 
         </>
