72330293a715f778c0835c7d04a68892cfcc5d5e
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-26 18:31:30 +0100

    add changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,8 @@ export default function NotFound() {
     return (
         <main className="text-center pt-4 vh-100">
             <h2 className=" mt-4 text-primary">Nessun Post Trovato</h2>
-            <Link to={'/posts'} className="btn btn-primary">Torna alla lista dei Posts</Link>
+            <div className="fs-5 mt-4"><strong>Il nostro Web Developer Miguel potrebbe non aver ancora aggiunto questa ricetta</strong></div>
+            <Link to={'/posts'} className="btn btn-primary mt-4">Torna alla lista dei Posts</Link>
         </main>
     )
 }
